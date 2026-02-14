94e915ebbd387bead7ebce6003fdb276c1d695c0
Suraj141203 <surajyadav141203@gmail.com>
2026-02-15 00:01:12 +0530

    done

---
+++ The Python/Variables and DataTypes/DataTypes.py
@@ -0,0 +1,17 @@
+# Here We Define How Many Types Of Data In Python
+
+a = 1  # It is an Integer Value 
+
+b = 2.4 # It is a floating Point Number
+
+c = "Suraj" # It is a String Value
+
+d = True # It is a Boolean Value Show 0 (True) and 1 (False) 
+
+e = None # It is a None When We Don't Want To Decide Variables Value We Using None
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
---
+++ The Python/Variables and DataTypes/Variables.py
@@ -0,0 +1,7 @@
+# Here a is variable where store 1 
+a = 1 
+b = 2
+#Here name also a variable but in this "Suraj" is different type of data 
+name = "Suraj"
+print(a + b) 
+print(name)
