5f71127f7b17e84a371160532aba85f7af4ad685
Suraj141203 <surajyadav141203@gmail.com>
2026-02-17 23:22:34 +0530

    done

---
+++ The Python/Strings/02_Nameshort.py
@@ -0,0 +1,9 @@
+name = "Suraj"
+
+nameshort = name[0:3] # Here Comes Only 0 to 3 index Data Because last index -1 if 
+# We Write 1:2 Only One Word Print Last index 2-1 = 1 
+print(nameshort)
+
+character1 = name[0]
+print(character1)
+
