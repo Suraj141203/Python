f7134831dccc39a6b91923455e59d981b280127f
Suraj141203 <surajyadav141203@gmail.com>
2026-02-25 22:18:05 +0530

    done

---
+++ The Python/Module Of Pythons/drawing.py
@@ -0,0 +1,11 @@
+import turtle
+t = turtle.Turtle()
+t.circle(50)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+turtle.done()
---
+++ The Python/Module Of Pythons/music_player.py.py
@@ -0,0 +1,7 @@
+import pygame
+
+pygame.mixer.init()
+pygame.mixer.music.load("Module Of Pythons\song.mp3")
+pygame.mixer.music.play()
+
+input("Press Enter to stop...")
