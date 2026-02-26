a5af0619696db31371b873d70a486f9133efea89
Suraj141203 <surajyadav141203@gmail.com>
2026-02-26 22:30:46 +0530

    done

---
+++ The Python/List And Tupple/03_tuple.py
@@ -0,0 +1,10 @@
+# Creating a tuple
+numbers = (10, 20, 30, 40, 50)
+
+print("Tuple:", numbers)
+print("First Element:", numbers[0])
+print("Last Element:", numbers[-1])
+
+student = ("Suraj", 20, 85.5, True)
+
+print("Student Data:", student) 
